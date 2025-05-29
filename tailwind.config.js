6f63bb4f57c3f1f80bd1e4b02f0c42931399e7be
Kacper Dęga <kacperdgf@o2.pl>
2025-05-29 10:01:03 +0200

    chore: packages update and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: [require("daisyui")]
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dracula"],
+  },
 };
