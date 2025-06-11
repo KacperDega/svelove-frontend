bea695d04298c80e1cdfc3ecb93649f93cb1b321
Kacper Dęga <kacperdgf@o2.pl>
2025-06-11 22:55:26 +0200

    feat: dynamic title added to routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 // src/App.tsx
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, {useEffect} from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Main from './pages/Main';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 
+const titleMap: Record<string, string> = {
+  '/': 'Strona główna | svelove',
+  '/login': 'Logowanie | svelove',
+  '/register': 'Rejestracja | svelove',
+  '/dashboard': 'Panel użytkownika | svelove',
+};
+
+function TitleUpdater() {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.title = titleMap[location.pathname] || 'svelove';
+  }, [location.pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+    <TitleUpdater />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
