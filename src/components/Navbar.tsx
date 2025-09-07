a6de9b2d1f6d5824e261db42e509eba74b37863c
Kacper Dęga <kacperdgf@o2.pl>
2025-09-07 14:44:32 +0200

    refactor: update navbar layout and improve centering

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,14 +15,15 @@ const Navbar: React.FC = () => {
   ];
 
   return (
-    <nav className="border-b border-secondary fixed bottom-0 left-0 right-0 flex justify-around p-4 text-sm shadow md:relative md:top-0 md:justify-start md:space-x-8 md:p-6 bg-neutral z-10">
-      <div className="hidden md:block mr-4">
-        <img src={logo} alt="Logo" className="h-6 max-h-6 object-contain" />
+    <nav className="border-b border-secondary fixed bottom-0 left-0 right-0 flex items-center justify-between h-[73px] px-4 text-sm shadow bg-neutral z-10
+                    md:relative md:top-0 md:px-6">
+
+      <div className="hidden md:block">
+        <img src={logo} alt="Logo" className="h-6 object-contain" />
       </div>
 
-      {navItems.map((item) => {
-        const isActive = location.pathname.startsWith(item.to);
-        return (
+      <div className="absolute left-1/2 -translate-x-1/2 flex space-x-6">
+        {navItems.map((item) => (
           <NavLink
             key={item.to}
             to={item.to}
