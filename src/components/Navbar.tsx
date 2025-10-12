77eda8e9000c96057f33092ee381500ddf42ead2
Kacper Dęga <kacperdgf@o2.pl>
2025-10-12 15:17:06 +0200

    feat: update Navbar to display app name instead of logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,11 +18,11 @@ const Navbar: React.FC = () => {
   ];
 
   return (
-    <nav className="border-b border-secondary fixed bottom-0 left-0 right-0 flex items-center justify-between h-[73px] px-4 text-sm shadow bg-neutral z-10
-                    md:relative md:top-0 md:px-6">
+    <nav className="border-b border-secondary fixed bottom-0 left-0 right-0 flex items-center justify-between h-[73px] px-4 text-sm shadow bg-neutral z-50
+                    md:relative md:top-0 md:z-10 md:px-6">
 
       <div className="hidden md:block">
-        <img src={logo} alt="Logo" className="h-6 object-contain" />
+        <h1 className="object-contain font-teaspoon text-primary text-4xl select-none">svelove</h1>
       </div>
 
       <div className="absolute left-1/2 -translate-x-1/2 flex space-x-6">
